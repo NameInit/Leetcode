656a08f1e41483cbb264545dc1010417bc12869d
bryenmar <bryenmar@student.21-school.ru>
2025-07-30 20:07:07 +0300

    2419

---
+++ 2419/2419.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+
+class Solution {
+public:
+    int longestSubarray(std::vector<int>& nums) {
+        int ans=1, temp_ans=1, max_num=nums[0], n=nums.size();
+		for(int i=1; i<n; ++i){
+			if(max_num<nums[i]){
+				max_num=nums[i];
+				ans=1;
+			}
+			temp_ans=((nums[i]==nums[i-1])&&(max_num==nums[i])) ? temp_ans+1 : 1;
+			if(temp_ans>ans)
+				++ans;
+		}
+		return ans;
+    }
+};
+
+
+int main(){
+	Solution test;
+	{
+		std::vector<int> nums={1,2,3,3,2,2}; //2
+		std::cout << test.longestSubarray(nums) << std::endl;
+	}
+	{
+		std::vector<int> nums={25,25,25,26}; //1
+		std::cout << test.longestSubarray(nums) << std::endl;
+	}
+	{
+		std::vector<int> nums={1,2,3,4}; //1
+		std::cout << test.longestSubarray(nums) << std::endl;
+	}
+	{
+		std::vector<int> nums={1,2,3,4,5,6,6}; //2
+		std::cout << test.longestSubarray(nums) << std::endl;
+	}
+	{
+		std::vector<int> nums={100,99,100,2}; //1
+		std::cout << test.longestSubarray(nums) << std::endl;
+	}
+	return 0;
+}
